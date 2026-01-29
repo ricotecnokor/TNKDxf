3e8c29bcb684b5f2f80043ecf6fd9f5787e5fe1d
ricotecnokor <ricardo.pinto@tecnokor.com.br>
2026-01-29 15:33:27 -0300

    Refatora: move ArquivoItem para arquivo separado
    A classe ArquivoItem foi extraída de ListViewDwgs.cs e movida para ArquivoItem.cs, mantendo o mesmo namespace. Essa reorganização melhora a estrutura do projeto e facilita a manutenção, sem alterar a lógica existente.

---
+++ TNKDxf/ViewModel/ArquivoItem.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TNKDxf.ViewModel
+{
+    public class ArquivoItem : INotifyPropertyChanged
+    {
+        private string _nome;
+        private bool _selecionado;
+        private bool _errado;
+        private bool _aberto;
+        private bool _enviado;
+        private bool _podeConverter = true;
+        private bool _podeBaixar = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Nome { get => _nome; set { _nome = value; OnPropertyChanged(); } }
+        public bool Selecionado { get => _selecionado; set { _selecionado = value; OnPropertyChanged(); } }
+        public bool Errado { get => _errado; set { _errado = value; OnPropertyChanged(); } }
+        public bool Aberto { get => _aberto; set { _aberto = value; OnPropertyChanged(); } }
+        public bool Enviado { get => _enviado; set { _enviado = value; OnPropertyChanged(); } }
+
+        // controla se o botão Converter está habilitado para esta linha
+        public bool PodeConverter { get => _podeConverter; set { _podeConverter = value; OnPropertyChanged(); } }
+        public bool PodeBaixar { get => _podeBaixar; set { _podeBaixar = value; OnPropertyChanged(); } }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- TNKDxf/ViewModel/ListViewDwgs.cs
+++ TNKDxf/ViewModel/ListViewDwgs.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using TNKDxf.Dominio.Dwgs;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace TNKDxf.ViewModel
 {
@@ -39,34 +37,6 @@ namespace TNKDxf.ViewModel
         public int ObterIndice(ArquivoItem arquivo)
         {
             return _colecaoDwgs.ObterIndice(arquivo.Nome);
-        }
-    }
-
-    public class ArquivoItem : INotifyPropertyChanged
-    {
-        private string _nome;
-        private bool _selecionado;
-        private bool _errado;
-        private bool _aberto;
-        private bool _enviado;
-        private bool _podeConverter = true;
-        private bool _podeBaixar = false;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public string Nome { get => _nome; set { _nome = value; OnPropertyChanged(); } }
-        public bool Selecionado { get => _selecionado; set { _selecionado = value; OnPropertyChanged(); } }
-        public bool Errado { get => _errado; set { _errado = value; OnPropertyChanged(); } }
-        public bool Aberto { get => _aberto; set { _aberto = value; OnPropertyChanged(); } }
-        public bool Enviado { get => _enviado; set { _enviado = value; OnPropertyChanged(); } }
-
-        // controla se o botão Converter está habilitado para esta linha
-        public bool PodeConverter { get => _podeConverter; set { _podeConverter = value; OnPropertyChanged(); } }
-        public bool PodeBaixar { get => _podeBaixar; set { _podeBaixar = value; OnPropertyChanged(); } }
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
